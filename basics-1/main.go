2f853bf792a2e576a1d8a8f678ff0eb219de54b2
Will Beason <willbeason@gmail.com>
2021-04-30 22:00:42 -0500

    finish first example

--- basics-1/main.go
+++ basics-1/main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
 )
 
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Pixel Rocks!",
 		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
 	}
 	win, err := pixelgl.NewWindow(cfg)
 	if err != nil {
 		panic(err)
 	}
+
+	win.Clear(colornames.Skyblue)
 
 	for !win.Closed() {
 		win.Update()
