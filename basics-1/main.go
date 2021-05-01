f60db6939cdc94e255ac556bd29df6722d13665c
Will Beason <willbeason@gmail.com>
2021-04-30 21:55:42 -0500

    start example 1

---
+++ basics-1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Pixel Rocks!",
+		Bounds: pixel.R(0, 0, 1024, 768),
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	for !win.Closed() {
+		win.Update()
+	}
+}
+
+func main() {
+	pixelgl.Run(run)
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module willbeason/learn-pixel
 
 go 1.16
 
-require github.com/faiface/pixel v0.10.0 // indirect
+require github.com/faiface/pixel v0.10.0
